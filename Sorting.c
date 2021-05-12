f72c8a668a7935958a0057da605839047f40313a
akib-islam-coder <akibislam2021@gmail.com>
2021-05-12 20:42:39 +0530

    Commit #4 - Insertion Sort Implementation
        1. Insertion sort implementation (adaptive and stable)
        2. Best case complexity - O(n) and worst case complexity - O(n^2)
    
        Signed-off-by - Akib Islam <akibislam2021@gmail.com>

--- Sorting.c
+++ Sorting.c
@@ -4,10 +4,10 @@
 
 #define NO_OF_DATA_ITEMS	10
 
-//int data_arr[NO_OF_DATA_ITEMS] = { 47, 75, 86, 100, 100, 1, 69, 67, 87, 47 };		//Unsorted array
-int data_arr[NO_OF_DATA_ITEMS] = { 10, 20, 30, 40, 50, 60, 70, 80, 90, 100 };		//Already sorted elements
+int data_arr[NO_OF_DATA_ITEMS] = { 47, 75, 86, 100, 100, 1, 69, 67, 87, 47 };		//Unsorted array
+//int data_arr[NO_OF_DATA_ITEMS] = { 10, 20, 30, 40, 50, 60, 70, 80, 90, 100 };		//Already sorted elements
 
-void swap(int * num1, int * num2)
+void swap(int* num1, int* num2)
 {
 	int tmp = 0;
 	tmp = *num1;
@@ -30,7 +30,7 @@ void bubble_sort(int data_arr[NO_OF_DATA_ITEMS])
 	/*
 	* Complexity of Bubble Sort - Best Time (when list is already sorted) - O(n), Worst time - O(n^2)
 	* Bubble sort implementation will be adaptive and stable (order will be maintained for duplicates)
-	* The bubble sort algorithm compares consecutive elements and after completing one pass, largest element will be seperated and pused at the
+	* The bubble sort algorithm compares consecutive elements and after completing one pass, largest element will be separated and pushed at the
 	* end of the array index.
 	* If there are n elements and we do k passes, then we will get the kth largest element after completing kth pass of bubble sort.
 	*/
@@ -43,7 +43,7 @@ void bubble_sort(int data_arr[NO_OF_DATA_ITEMS])
 	bool swap_done = false;
 
 	int i = 0;
-	for (pass = 0; pass < NO_OF_DATA_ITEMS-1; pass++)	//Loop for no of passes - (n-1) pass required
+	for (pass = 0; pass < NO_OF_DATA_ITEMS - 1; pass++)	//Loop for no of passes - (n-1) pass required
 	{
 		swap_done = false;
 		for (i = 0; i < NO_OF_DATA_ITEMS - 1 - pass; i++)
@@ -68,23 +68,67 @@ void bubble_sort(int data_arr[NO_OF_DATA_ITEMS])
 
 }
 
+void insertion_sort(int data_arr[NO_OF_DATA_ITEMS])
+{
+
+	/*
+	* Complexity of Insertion Sort - Best Time (when list is already sorted) - O(n), Worst time - O(n^2)
+	* Insertion sort implementation will be adaptive and stable (order will be maintained for duplicates)
+	* Insertion sort is most helpful while sorting the linkedlist.
+	* Insertion sort intermediate values don't convey any useful information unlike bubble sort.
+	*/
+
+	/* Print the array*/
+	printf("Elements in array before insertion sort : \r\n");
+	print_elements(data_arr);
+
+	int i = 0, j = 0;
+	int num = 0;
+
+	for (i = 1; i < NO_OF_DATA_ITEMS; i++)
+	{
+		num = data_arr[i];
+
+		for (j = i - 1; j >= 0; j--)
+		{
+			if (num < data_arr[j])
+			{
+				data_arr[j + 1] = data_arr[j];	//Shift right
+			}
+			else
+			{
+				break;
+			}
+		}
+		data_arr[j + 1] = num;
+	}
+
+	/* Print the array*/
+	printf("Elements in array after insertion sort : \r\n");
+	print_elements(data_arr);
+}
+
 int main(void)
 {
 	int input;
 
 	printf("Enter the sorting algorithm\r\n"
-		"1.  Bubble Sort\r\n");
+		"1. Bubble Sort\r\n"
+		"2. Insertion Sort\r\n");
 	scanf_s("%d", &input);
 
 	switch (input)
 	{
-		case 1:
-			bubble_sort(data_arr);
-			break;
+	case 1:
+		bubble_sort(data_arr);
+		break;
 
+	case 2:
+		insertion_sort(data_arr);
+		break;
 
-		default:
-			break;
+	default:
+		break;
 	}
 
 
